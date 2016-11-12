9087d7352beb85092767fd5d8a2d20e51029c6b8
sunkay <sunkay11@gmail.com>
2016-11-12 12:02:34 -0500

    lists

--- lists.ex
+++ lists.ex
@@ -1,7 +1,33 @@
 defmodule MyList do
- def sum([], total), do: total
- def sum([head|tail], total), do: (
-   sum(tail, head+total)
-   IO.puts tail
-  )
+ def sum(list), do: _sum(list, 0)
+
+ defp _sum([], total), do: total
+ defp _sum([head|tail], total), do: (_sum(tail, head+total))
+
+ def wt([], _target), do: []
+ def wt([head = [_,target,_,_] | tail] , target), do: (
+  [[head] | wt(tail, target)]
+ )
+ def wt([ _ | tail ], target), do: wt(tail, target)
+
+
+ def span(from, to), do: _span(from, to, [])
+ def _span(from, to, list) when from==to do
+   list ++ [to]
+ end
+ def _span(from, to, list) do
+   list ++ [from | _span(from+1, to, list)]
+ end
+
+
+ def test_data do
+   [
+     [22334, 27, 15, 1.25],
+     [22334, 22, 15, 1.25],
+     [22334, 27, 15, 1.25],
+     [22334, 27, 15, 1.25],
+     [22334, 25, 15, 1.25]
+   ]
+ end
+
 end
